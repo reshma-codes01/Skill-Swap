35cf380c64294da81e692eb2f592069e4aa293cb
Reshma <reshmay481@gmail.com>
2026-04-13 17:56:07 +0530

    done

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
+const URL = "https://skill-swap-xkoj.onrender.com";
 
 export const socket = io(URL, {
     autoConnect: false, // Connect manually when needed
